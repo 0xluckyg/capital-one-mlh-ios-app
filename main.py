ec2f45fb5dbaec620a1cf2717a7fd4ee4dcdb99c
Jia Wu <ratedrion@gmail.com>
2016-08-07 09:29:56 -0400

    made yelp get if address exists

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from yelp.oauth1_authenticator import Oauth1Authenticator
 app = Flask(__name__)
 
 merchants = 'http://api.reimaginebanking.com/merchants/'
-purchases = 'http://api.reimaginebanking.com/accounts/57a6cea4bc727e4b519f21b9/purchases?key='
+purchases = 'http://api.reimaginebanking.com/accounts/57a6bd19bc727e4b519f20a6/purchases?key='
  
 def load_credentials():
     Config = ConfigParser.ConfigParser()
@@ -22,7 +22,7 @@ def load_credentials():
     creds.append(Config.get('API_Keys', 'Token_Secret'))
     return creds
 
-def is_restaurant(city, address, key,skey,tok, stok):
+def is_restaurant(street, city, key, skey, tok, stok):
     auth = Oauth1Authenticator(
     consumer_key=key,
     consumer_secret=skey,
@@ -31,40 +31,37 @@ def is_restaurant(city, address, key,skey,tok, stok):
     )
     client = Client(auth)
     params = {
-        'location.address':address,
+        'location.address':street,
         'term': 'food'
     }
-    print client.search(city, **params)
-    if len(client.search(city, **params)) != 0:
-    	return True
+    data = client.search(city, **params)
+    for x in data.businesses:
+        if street == x.location.address[0]:
+            return True
     return False
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/api')
 def get_restaurants():
-    valid_merchant_ids = []
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    valid_merchant_ids = {}
     creds = load_credentials()
     cone_key = creds[0]
-    cons_key = creds[1]
-    cons_secret = creds[2]
-    token = creds[3]
-    sec_token = creds[4]
     responses = requests.get(purchases+cone_key).json()
-    for x in range(len(responses)):
-        merchant_id = responses[x]['merchant_id']
-        print merchants+merchant_id+'/?key='+cone_key
-        address = requests.get(merchants+merchant_id+'/?key='+cone_key).json()['address']
-        print address
-	city = address['city']
-	print city
-        if is_restaurant(city, address, cons_key, cons_secret, token, sec_token):
-            valid_merchant_ids.append(merchant_id)
+    for x in responses:
+        merchant_id = x['merchant_id']
+        data = requests.get(merchants+merchant_id+'/?key='+cone_key).json()
+        print data
+        street = data['address']['street_number'] + " " + data['address']['street_name']
+        city = data['address']['city']
+        if is_restaurant(street, city, creds[1], creds[2], creds[3], creds[4]):
+            valid_merchant_ids[merchant_id] = x['amount']
     print valid_merchant_ids
-    return "test"
+    return json.dumps(valid_merchant_ids)
 
 if __name__ == '__main__':
 	app.debug = True
